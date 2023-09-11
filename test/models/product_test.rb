b493d0609a168a95b76f89a497c0ea3aa9673bb7
Lexus <oleksii.kuts@faceit-team.com>
2023-09-11 10:53:20 +0300

    product MiniTests image_url section

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,7 +11,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title:        "My ook Title",
+    product = Product.new(title:        "My book Title",
                           description:  "yyy",
                           image_url:    "zzz.jpg")
     product.price = -1
@@ -25,4 +25,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  def new_product(image_url)
+    Product.new(title:        "My book Title",
+                description:  "yyy",
+                price:        1,
+                image_url:    image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg​ http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} must be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} must be invalid"
+    end
+  end
+
 end
