8a21bf7acb38a09c2c71d92f32d47ab2ef86550d
Lexus <oleksii.kuts@faceit-team.com>
2023-09-13 12:06:06 +0300

    comment system/products_spec.rb

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe 'Products', type: :system do
-  fixtures :products
-
-  before do
-    @product = products(:one)
-  end
-
-  it 'visits the index' do
-    visit products_url
-    expect(page).to have_selector("h1", text: "Products")
-  end
-
-  it 'should create a product' do
-    visit products_url
-    click_on "New product"
-
-    fill_in "Description", with: @product.description
-    fill_in "Image url", with: @product.image_url
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: "New Product"
-    click_on "Create Product"
-
-    expect(page).to have_text("Product was successfully created")
-  end
-
-  it 'should update a product' do
-    visit product_url(@product)
-    click_on "Edit this product", match: :first
-
-    fill_in "Description", with: @product.description
-    fill_in "Image url", with: @product.image_url
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: "Updated Product"
-    click_on "Update Product"
-
-    expect(page).to have_text("Product was successfully updated")
-  end
-
-  it 'should destroy a product' do
-    visit product_url(@product)
-    click_on "Destroy this product", match: :first
-
-    expect(page).to have_text("Product was successfully destroyed")
-  end
-end
+# RSpec.describe 'Products', type: :system do
+#   fixtures :products
+#
+#   before do
+#     @product = products(:one)
+#   end
+#
+#   it 'visits the index' do
+#     visit products_url
+#     expect(page).to have_selector("h1", text: "Products")
+#   end
+#
+#   it 'should create a product' do
+#     visit products_url
+#     click_on "New product"
+#
+#     fill_in "Description", with: @product.description
+#     fill_in "Image url", with: @product.image_url
+#     fill_in "Price", with: @product.price
+#     fill_in "Title", with: "New Product"
+#     click_on "Create Product"
+#
+#     expect(page).to have_text("Product was successfully created")
+#   end
+#
+#   it 'should update a product' do
+#     visit product_url(@product)
+#     click_on "Edit this product", match: :first
+#
+#     fill_in "Description", with: @product.description
+#     fill_in "Image url", with: @product.image_url
+#     fill_in "Price", with: @product.price
+#     fill_in "Title", with: "Updated Product"
+#     click_on "Update Product"
+#
+#     expect(page).to have_text("Product was successfully updated")
+#   end
+#
+#   it 'should destroy a product' do
+#     visit product_url(@product)
+#     click_on "Destroy this product", match: :first
+#
+#     expect(page).to have_text("Product was successfully destroyed")
+#   end
+# end
 
 __END__
 require "application_system_test_case"
