b2667b38376407d99accc148a620ec8717b6fc2f
Lexus <oleksii.kuts@faceit-team.com>
2023-09-15 09:08:05 +0300

    store_controller_spec.rb: add minitest version of code

--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe StoreController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
       puts "response.body = #{response.body}"
-      # expect(response.body).to have_selector('nav a', minimum: 4)
-      # expect(response.body).to have_selector('main ul li', 3)
-      # expect(response.body).to have_selector('h2', 'Programming Ruby 1.9')
-      # expect(response.body).to have_selector('div', /\$[,\d]+\.\d\d/)
+      expect(response.body).to have_selector('nav a', minimum: 4)
+      expect(response.body).to have_selector('main ul li', 3)
+      expect(response.body).to have_selector('h2', 'Programming Ruby 1.9')
+      expect(response.body).to have_selector('div', /\$[,\d]+\.\d\d/)
     end
   end
 end
@@ -19,5 +19,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '​nav a'​, minimum: 4
+    assert_select 'main ul li', 3
+    assert_select '​h2'​, 'Programming Ruby 1.9'​
+    assert_select '​div'​, /​\$[,\d]+\.\d\d/​
   end
 end
