9d15c24304c4fefca8bb75027461c6e7140afe4d
Lexus <oleksii.kuts@faceit-team.com>
2023-09-15 10:28:52 +0300

    enable render_view

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Enable view rendering for all controller tests
+  config.render_views
 end
