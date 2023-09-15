40bdfc5c57bacde07985a8df4c266f75d8fb5cae
KutsOleksii <oleksii.kuts@faceit-team.com>
2023-09-15 15:11:37 +0000

    cart scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_065606) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_15_142103) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
