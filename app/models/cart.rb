52c75467984062c42e1ce5ea33690f48b411098c
Lexus <oleksii.kuts@faceit-team.com>
2023-09-18 16:20:59 +0300

    cart.rb -> add console.logger.debug

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,8 +5,10 @@ class Cart < ApplicationRecord
     current_item = line_items.find_by(product_id: product.id)
     if current_item
       current_item.quantity += 1
+      Rails.logger.debug("Incremented Quantity: #{current_item.quantity}")
     else
       current_item = line_items.build(product_id: product.id)
+      Rails.logger.debug("New Line Item Created")
     end
     current_item
   end
