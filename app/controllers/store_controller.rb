8cc6a7f102d582ebe84cdf700fe97344d5ef5701
Lexus <oleksii.kuts@faceit-team.com>
2023-09-18 10:47:07 +0300

    qf index_counter #2

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     session[:index_counter] ||= 0
-    session[:index_counter] += 0
+    session[:index_counter] += 1
     # @counter = session[:index_counter]
   end
 end
