39aa3f65df3feaa6c786f56d133586b166c930eb
Lexus <oleksii.kuts@faceit-team.com>
2023-09-18 18:01:57 +0300

    qf spec line_items_controller

--- spec/requests/line_items_controller_spec.rb
+++ spec/requests/line_items_controller_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "LineItems", type: :request do
     # expect(response.body).to have_selector('h2', text: 'Programming Ruby 1.9')
     # expect(response.body).to have_selector('div', text: /\$[,\d]+\.\d\d/)
     expect(response.body).to include('Your Pragmatic Cart')
-    expect(response.body).to include("1 × Programming Ruby 1.9")
+    expect(response.body).to include("1 \u00d7 Programming Ruby 1.9")
     # expect(response.body).to have_selector('h2', text: 'Your Pragmatic Cart')
     # expect(response.body).to have_selector('li', text: "1 × Programming Ruby 1.9")
   end
