3aef90a0e83a0838a0d65c992508f77104be95dd
Lexus <oleksii.kuts@faceit-team.com>
2023-09-20 10:52:36 +0300

    spec line_items_controller -> rails.logger

--- spec/requests/line_items_controller_spec.rb
+++ spec/requests/line_items_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "LineItems", type: :request do
 
     follow_redirect!
 
-    Rails.logger.debug(response.body)
+    Rails.logger.debug(response.body.to_s)
     expect(response.body).to have_selector('h2', text: "Your Cart")
     expect(response.body).to have_selector('td', text: "Programming Ruby 1.9")
   end
