583fb3f81b5dd21babc7b35ca9711d2187444a1e
Lexus <oleksii.kuts@faceit-team.com>
2023-09-20 08:03:09 +0300

    migration: price in line_items -> up and down methods implemented

--- db/migrate/20230920044534_copy_product_price_into_line_items.rb
+++ db/migrate/20230920044534_copy_product_price_into_line_items.rb
@@ -1,4 +1,18 @@
 class CopyProductPriceIntoLineItems < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    # get price from products_table and put it into line_items
+    LineItem.all.each do |line_item|
+      product_id = line_item.product_id
+      product = Product.find(product_id)
+      line_item.price = product.price
+      line_item.save
+    end
+  end
+
+  def down
+    # Remove the copied prices from line_items
+    LineItem.all.each do |line_item|
+      line_item.price = nil
+      line_item.save
   end
 end
