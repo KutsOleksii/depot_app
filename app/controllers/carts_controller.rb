77dd79d325af214c10c66198f4341dd82371298f
Lexus <oleksii.kuts@faceit-team.com>
2023-09-21 09:15:21 +0300

    prevent accessing other`s cart#4

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
-  pp session
 
   # GET /carts or /carts.json
   def index
@@ -10,6 +9,7 @@ class CartsController < ApplicationController
 
   # GET /carts/1 or /carts/1.json
   def show
+    pp session
   end
 
   # GET /carts/new
