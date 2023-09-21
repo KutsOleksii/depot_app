1006c9156440dd49c2d87b653f4fc2715f4c1f1a
Lexus <oleksii.kuts@faceit-team.com>
2023-09-21 13:28:37 +0300

    add to cart -> redirect to index page

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+        format.html { redirect_to store_index_url }
         # format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
       else
