f0409fbef9653361e530fa0b43fbf9d4aa798d77
Lexus <oleksii.kuts@faceit-team.com>
2023-09-21 16:46:11 +0300

    turbo in cart rendering

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,13 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            :cart,
+            partial: 'layout/cart',
+            locals: { cart: @cart }
+          )
+        end
         format.html { redirect_to store_index_url }
         # format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
