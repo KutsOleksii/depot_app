932de1d185f789405b8580cf03797b77c6d6b92a
Lexus <oleksii.kuts@faceit-team.com>
2023-09-22 10:12:09 +0300

    add TurboLinks::Controller to config.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,10 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.generators.test_framework = :rspec
-    
+    config.to_prepare do
+      ActionController::Base.include Turbolinks::Controller
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
