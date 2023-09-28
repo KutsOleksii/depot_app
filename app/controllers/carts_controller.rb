d2b24a7075bf1456ef7271f9387fc21a75cd3e2f
Lexus <oleksii.kuts@faceit-team.com>
2023-09-28 08:39:05 +0300

    remove notice while deleting cart (it doesn`t work)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,7 +58,7 @@ class CartsController < ApplicationController
         render turbo_stream: turbo_stream.replace(
           :cart,
           partial: 'layouts/cart',
-          locals: { cart: @cart,  notice: "Your cart is currently empty" }
+          locals: { cart: @cart }
         )
       end
       format.html { redirect_to store_index_url, notice: "Your cart is currently empty" }
