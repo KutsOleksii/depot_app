b1f3e6b7c51a66dd018419788a81bb3529195b86
Lexus <oleksii.kuts@faceit-team.com>
2023-09-28 15:43:29 +0300

    try to highlight product when modified

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,6 +42,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
 
+        @modifyed_product = @product
         @product.broadcast_replace_later_to 'products', partial: 'store/product'
       else
         format.html { render :edit, status: :unprocessable_entity }
